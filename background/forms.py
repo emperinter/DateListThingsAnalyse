71d0d648648511840733a41cb3eef1afcab8b4d1
emperinter <hehiuzhang@gmail.com>
2022-02-10 14:13:08 +0800

    mv admin register code to admin.py

--- background/forms.py
+++
@@ -1,9 +0,0 @@
-#froms.py通过模型创建表单
-
-from .models import ModelFileField
-from django.forms import ModelForm
-
-class ModelFormFile(ModelForm):
-    class Meta:
-        model = ModelFileField
-        fields = "__all__"
--- front/admin.py
+++ front/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from background.models import User,ListThings
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(ListThings)
