8aa36111f8432402f5394123b473a2c2a2b699fc
emperinter <hehiuzhang@gmail.com>
2022-02-11 17:24:52 +0800

    add a customer router

---
+++ api/self_router.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+from . import views
+
+# 用于设置数据获取地址
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'things', views.ThingsViewSet)
+router.register(r'get-user', views.UserListView,basename='get-user')
+router.register(r'get-things', views.ThingsListView,basename='get-things')
--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,13 @@
 from django.conf.urls import url
 from django.urls import path, include
-from rest_framework import routers
-from . import views
 from rest_framework.documentation import include_docs_urls
 
-# Routers provide an easy way of automatically determining the URL conf.
-# 用于设置数据获取地址
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'things', views.ThingsViewSet)
-router.register(r'get-user', views.UserListView,basename='get-user')
-router.register(r'get-things', views.ThingsListView,basename='get-things')
+from . import self_router
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(self_router.router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include_docs_urls(title='DateListThingsAPI DOCS')),   # 参数为title为接口文档网站的标题
 ]
